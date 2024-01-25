6ea84ad25d14c42ad295bb81f7206582146fad46
Swastikyadav <swastikb380@gmail.com>
2024-01-25 14:14:42 +0530

    Configure classname productionPrefix for marketing and container

--- mfe-app-2/marketing/src/App.js
+++ mfe-app-2/marketing/src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 
 import LandingPage from "./components/Landing";
 import PricingPage from "./components/Pricing";
 
+/**
+ * Configures the class name generator utility from Material UI Styles. 
+ * Sets the production prefix to "ma" which will prefix all 
+ * generated class names in production.
+ */
+const generateClassName = createGenerateClassName({
+    productionPrefix: "ma",
+});
+
 export default () => {
     return (
         <div>
-            <StylesProvider>
+            <StylesProvider generateClassName={generateClassName}>
                 <Router>
                     <Switch>
                         <Route exact path="/pricing" component={PricingPage} />
