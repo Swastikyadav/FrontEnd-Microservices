92997fc354be90511d4b67457e60824c633e4f2e
Swastikyadav <swastikb380@gmail.com>
2024-01-31 17:30:01 +0530

    Use browserHistory in development else use memoryHistory

--- mfe-app-2/marketing/src/bootstrap.js
+++ mfe-app-2/marketing/src/bootstrap.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createMemoryHistory } from "history";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
 // Mount function to start up the app.
-const mount = (elm, { onNavigate }) => {
-    const history = createMemoryHistory();
+const mount = (elm, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
 
     if (onNavigate) {
         history.listen(onNavigate);
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === "development") {
     const devRoot = document.querySelector("#marketing_dev_root");
 
     if (devRoot) {
-        mount(devRoot, {});
+        mount(devRoot, { defaultHistory: createBrowserHistory() });
     }
 }
 
