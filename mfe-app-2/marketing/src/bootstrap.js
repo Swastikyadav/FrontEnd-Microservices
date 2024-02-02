38538f68eba0369a7f921dfc3a245685f8a199fc
Swastikyadav <swastikb380@gmail.com>
2024-02-02 15:56:12 +0530

    Integrate auth app in container

--- mfe-app-2/marketing/src/bootstrap.js
+++ mfe-app-2/marketing/src/bootstrap.js
@@ -4,8 +4,10 @@ import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
 // Mount function to start up the app.
-const mount = (elm, { onNavigate, defaultHistory }) => {
-    const history = defaultHistory || createMemoryHistory();
+const mount = (elm, { onNavigate, defaultHistory, initialPath }) => {
+    const history = defaultHistory || createMemoryHistory({
+        initialEntries: [initialPath]
+    });
 
     if (onNavigate) {
         history.listen(onNavigate);
